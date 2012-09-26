667abd0d4c5606e8666dd6c571b6a6adcecab1bf
segfault <s9gf4ult@gmail.com>
2012-09-26 16:20:17 +0600

    * workable in mobile device

--- js/yamap.js
+++ js/yamap.js
@@ -2,8 +2,20 @@
 /// var yPoint1 = {lat : 24.424,
 ///                lon : 24}
 /// var yPoint2 = same as yPoint1
+jQuery(document).ready(function() {
 
+    function recalcMap() {
+        el = jQuery('#yandex_map_element');
+        if (el.width() == 0 | el.height() == 0) {
+            d = jQuery(document);
+            p = jQuery('.content > .post');
+            el.width(p.width() - 20);
+            el.height(d.height() * 0.8);
+        }
+    }
 
+    recalcMap();
+        
 ymaps.ready(function() {
     var map;
     function makeMap(p) {
@@ -23,11 +35,17 @@ ymaps.ready(function() {
     }
 
     function exec() {
+        if (map != null && map != undefined) {
+            x = map;
+            map = null;
+            x.destroy();
+        }
+        
         if (yAction == "baloon") {
             if (yPoint1 == 0) {
                 alert("This page needs 'lat' and 'lon' request parameters as latitude and longitude respectively");
             } else {
-                var map = makeMap(yPoint1);
+                map = makeMap(yPoint1);
                 var pm = new ymaps.Placemark([yPoint1.lat, yPoint1.lon]);
                 map.geoObjects.add(pm);
             } 
@@ -35,7 +53,7 @@ ymaps.ready(function() {
             if (yPoint1 == 0 || yPoint2 == 0) {
                 alert("This page needs 'lat', 'lon', 'plat', 'plon' request parameters as latitude and longitude respectively");
             } else {
-                var map = makeMap(yPoint2);
+                map = makeMap(yPoint2);
                 ymaps.route([[yPoint1.lat, yPoint1.lon], [yPoint2.lat, yPoint2.lon]],
                            {mapSetAutoApply: true})
                     .then(function(route) {
@@ -44,11 +62,32 @@ ymaps.ready(function() {
                     }, function(error) {
                         yPoint1 = yPoint2;
                         action = "baloon";
-                        map.destroy();
                         exec();
                     });
             }
         }
     }
+
+    function redrawMap() {
+        recalcMap();
+        exec();
+    }
+    
+    var lastfire = new Date();
+    var supportsOrientationChange = "onorientationchange" in window,
+    orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
+
+    window.addEventListener(orientationEvent, function() {
+        var nowfire = new Date();
+        if ((nowfire - lastfire) / 1000 > 1) {
+            lastfire = new Date();
+            // redrawMap();
+            window.location.reload(); // lol
+        }
+    });
+    
     exec()
 })
+
+        
+       })                       // Jquery ready
